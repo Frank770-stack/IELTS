e6573d4fc7acfaf46bdc385ce77da1576d2526f7
Frank Mugendi <njaombefrank@gmail.com>
2025-01-28 04:34:02 +0300

    Result management system

--- Client/src/lib/axios.js
+++ Client/src/lib/axios.js
@@ -1,5 +1,13 @@
+// import axios from "axios";
+// const backendUrl = "http://localhost:5000";
+
+// export const axiosInstance = axios.create({
+//   baseURL: `${backendUrl}/api`,
+//   withCredentials: true,
+// });
+
 import axios from "axios";
-const backendUrl = "http://localhost:5000";
+const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
 export const axiosInstance = axios.create({
   baseURL: `${backendUrl}/api`,
