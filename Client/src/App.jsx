3af4168c4475a9d41c05d0a428adb4603ef14776
Frank Mugendi <njaombefrank@gmail.com>
2025-02-28 20:24:53 +0300

    updated cors

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -13,6 +13,7 @@ import RecentResults from "./components/recent/recentresults.jsx";
 import Score from "./components/score/score.jsx";
 import Footer from "./components/Footer/Footer.jsx";
 import "./App.css";
+
 const App = () => {
   const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
 
