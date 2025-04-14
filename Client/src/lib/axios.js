a6380b631bff6390de42c0c492c10a4cb44b34f6
Frank Mugendi <njaombefrank@gmail.com>
2025-04-14 15:45:40 +0300

    minor changes

--- Client/src/lib/axios.js
+++ Client/src/lib/axios.js
@@ -1,15 +1,15 @@
-import axios from "axios";
-const backendUrl = "http://localhost:9000";
-
-export const axiosInstance = axios.create({
-  baseURL: `${backendUrl}/api`,
-  withCredentials: true,
-});
-
 // import axios from "axios";
-// const backendUrl = import.meta.env.VITE_BACKEND_URL;
+// const backendUrl = "http://localhost:9000";
 
 // export const axiosInstance = axios.create({
 //   baseURL: `${backendUrl}/api`,
 //   withCredentials: true,
 // });
+
+import axios from "axios";
+const backendUrl = import.meta.env.VITE_BACKEND_URL;
+
+export const axiosInstance = axios.create({
+  baseURL: `${backendUrl}/api`,
+  withCredentials: true,
+});
